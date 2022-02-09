9b0babacc630bd89947d1e6250a7654d11b1b75e
Gorjan <sekerinov@gmail.com>
2022-02-09 17:15:50 +0900

    first commit

---
+++ reference/path_demo.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+// Base file name
+console.log(path.basename(__filename));
+
+// Directory name
+console.log(path.dirname(__filename));
+// console.log(__dirname);
+
+// // File extension
+console.log(path.extname(__filename));
+
+// Create path object
+console.log(path.parse(__filename).base);
+
+// Concatenate paths
+console.log(path.join(__dirname, 'save', 'index.html'));
+
